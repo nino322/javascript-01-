c1b60049e7e11454796cebb160dd5bce65bead60
nino322 <55630809+nino322@users.noreply.github.com>
2023-05-11 17:35:43 +0400

    javascript 01

---
+++ javascript 01/script.js
@@ -0,0 +1,15 @@
+ var salary = prompt("Enter your price");
+
+ parent()
+
+ var income = 600;
+
+ if (income < 70){
+    alert ("xelfasi kargia");
+ }  
+ else if (income > 70 &&   income < 600){
+    alert("<h1>sashvaloa!</h1>");
+ }
+
+
+
---
+++ script.js
@@ -0,0 +1,3 @@
+console.log("Hello world");
+alert("Hello World ");
+document.write("<h1>Hello World</h1>");
